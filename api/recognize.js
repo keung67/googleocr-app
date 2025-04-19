744f79639fe0e50a4837a774a7ccd751fc2288b0
keung67 <127957412+keung67@users.noreply.github.com>
2025-04-20 03:08:46 +0800

    Update recognize.js

--- api/recognize.js
+++ api/recognize.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({
-      model: "gemini-2.0-pro-exp-02-05",
+      model: "gemini-2.5-flash-preview-04-17",
       generationConfig: {
         temperature: 1,
         topP: 0.95,
